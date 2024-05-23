55129c874bac4af78e31aa39d16fd58d9005948d
Rafi 2cev Tucev <tucev@hotmail.com>
2024-05-23 10:59:02 +0200

    first commit

---
+++ src/services/productservice.js
@@ -0,0 +1,11 @@
+export async function getProducts(category) {
+    const response = await fetch(baseUrl + "products?category=" + category);
+    if(response.ok) return response.json();
+    throw response;
+}
+
+export async function getProduct(id) {
+    const response = await fetch(baseUrl + "products/" + id);
+    if(response.ok) return response.json();
+    throw response;
+}
